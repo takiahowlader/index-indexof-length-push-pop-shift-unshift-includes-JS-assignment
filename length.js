3ef96e45324e078b03df7bfec88cdee773e8fcd9
takiahowlader <takiahowlader4466@gmail.com>
2024-10-15 22:42:32 +0600

    first commit

---
+++ length.js
@@ -0,0 +1,60 @@
+// cheak length
+const numbers =[1, 2, 3, 50, 55, 60, 70]
+// console.log(numbers.length);
+// cheak length
+
+
+// cheack index
+// console.log(numbers[4]);
+// cheack index
+
+// cheack indexof
+// console.log(numbers.indexOf(50));
+// cheack indexof
+
+
+// cheack push
+// numbers.push(76);
+// console.log(numbers);
+// cheack push
+
+
+// cheack pop
+// numbers.pop("70");
+// console.log(numbers);
+// cheack pop
+
+// cheack shift
+const team = ["Takia","Rima","Mitu","Subarna","Liya"]
+// team.shift()
+// console.log(team);
+// cheack shift
+
+// cheack unshift
+// team.unshift("mim")
+// console.log(team);
+// cheack unshift
+
+// cheack push
+// team.push("Tasnim");
+// console.log(team);
+// cheack push
+
+
+// cheak pop
+// team.pop("Liya");
+// console.log(team);
+// cheak pop
+
+
+
+// cheack includes
+// console.log(team.includes("Takia"));
+// console.log(team.includes("Takrim"));
+// console.log(Array.isArray(team));
+
+// cheack includes
+
+
+
+
